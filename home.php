6b26be486eeba09ca519647f83490bc066a4bceb
nkaffine <nicholas.kaffine@gmail.com>
2017-09-28 21:03:56 -0400

    Visual improvements for home page

--- home.php
+++ home.php
@@ -17,8 +17,32 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="styleSheets/main.css">
         <script src="scripts/main.js"></script>
+        <script src="scripts/style.js"></script>
     </head>
     <body>
-        <h1>Yay it worked</h1>
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="/home.php">NEU Todo</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="home.php">Home</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="col-lg-3 col-lg-offset-2 panel panel-default">
+            <h1 class="col-lg-10 noPad title">My Lists</h1>
+            <button type="button" class="close addBtn" style="font-size: 40px;" id="newList">+</button>
+            <div id="lists">
+
+            </div>
+        </div>
+        <div class="col-lg-5 panel panel-default">
+            <h1 class="col-lg-10 noPad title">Items</h1>
+            <button type="button" class="close addBtn" style="font-size: 40px;" id="newItem">+</button>
+            <div id="items">
+
+            </div>
+        </div>
     </body>
 </html>
